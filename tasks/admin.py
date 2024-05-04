df52a3c07a4b443c4bda13b6345f060774eb3de4
Javier971020 <102177276+Javier971020@users.noreply.github.com>
2024-05-04 17:02:07 +0200

    agregando cambios

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import *
 
 
 # Register your models here.
-
+# admin.site.register(BaseModel)
+#admin.site.register(CustomUserManager)
+admin.site.register(User,UserAdmin)
 admin.site.register(Municipios)
 admin.site.register(Provincias)
 admin.site.register(Propietarios)
--- tasks/serializer.py
+++ tasks/serializer.py
@@ -1,6 +1,41 @@
 from rest_framework import serializers
 from .models import *
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import get_user_model
 
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        refresh_token = self.get_token(self.user)
+
+        # Agrega los datos adicionales del usuario en la respuesta
+        data['user'] = {
+            'id': self.user.id,
+            'username': self.user.username,
+            'email': self.user.email,
+            # Agrega otros campos adicionales que desees incluir en la respuesta
+        }
+
+        data['refresh_token'] = str(refresh_token)
+        data['access_token'] = str(refresh_token.access_token)
+
+        return data
 
 class MunicipiosSerializer(serializers.ModelSerializer):
     class Meta:
